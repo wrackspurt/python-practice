7ffe1ad15e25f35d153ec866aaa4368e05b0f72e
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-12-17 19:19:10 +0300

    Revert "added implementations of text_wrap.py without using textwrap"
    This reverts commit 2dc516a6d9080ff023e535ce6e035abccf09d35b.

--- strings/text_wrap.py
+++ strings/text_wrap.py
@@ -4,7 +4,7 @@ You are given a string S and width w.
 Your task is to wrap the string into a paragraph of width w.
 """
 
-# solution 1
+
 import textwrap
 
 
@@ -16,31 +16,3 @@ if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
-
-
-"""
-# solution 2
-def wrap(string, max_width):
-    l = []
-    for i in range(0, len(string), max_width):
-        l.append(string[i:i + max_width])
-    return '\n'.join(l)
-
-
-if __name__ == '__main__':
-    string, max_width = input(), int(input())
-    result = wrap(string, max_width)
-    print(result)
-"""
-
-"""
-# solution 3
-def wrap(string, max_width):
-    return '\n'.join([string[i:i + max_width] for i in range(0, len(string), max_width)])
-
-
-if __name__ == '__main__':
-    string, max_width = input(), int(input())
-    result = wrap(string, max_width)
-    print(result)
-"""
