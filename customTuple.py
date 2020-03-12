3d50f67c485a7d570b45b40c29999f241f1b616f
Lisa <fanciful.mischief@gmail.com>
2020-03-12 23:21:02 +0300

    added the solution for the task about tuple

---
+++ customTuple.py
@@ -0,0 +1,47 @@
+"""
+Task 1
+Как известно, мы не всегда можем использовать tuple в качестве ключа словаря, т.к. он может включать в себя нехэшируемые
+объекты. Задание - написать свою реализацию tuple, которая решает эту проблему. Лучше решать с помощью collections.abc.
+Реализация хэш-функции может быть любой, лишь бы работала.
+"""
+
+from collections.abc import Hashable, Sequence
+from typing import Any
+
+
+class CustomTuple(Sequence, Hashable):
+    def __init__(self, *elements):
+        self.data = tuple(elements)
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        return f'{self.data}'
+
+    def __hash__(self):
+        lst = list(self.data)
+        for i in range(0, len(lst)):
+            if type(lst[i]) == list or type(lst[i]) == set:
+                lst[i] = tuple(lst[i])
+            elif type(lst[i]) == dict:
+                lst[i] = tuple(lst[i].items())
+        return hash(tuple(lst))
+
+    def count(self, x: Any) -> int:
+        c = 0
+        for i in range(0, len(self.data)):
+            if x == self.data[i]:
+                c += 1
+        return c
+
+    def index(self, x: Any, start: int = ..., end: int = ...) -> int:
+        for i in range(start, end):
+            if x == self.data[i]:
+                return i
