0081b1cccfe6be30c178e9de780855c3c250d5ad
Lisa <fanciful.mischief@gmail.com>
2020-03-13 17:17:01 +0300

    fixed decorator.py

--- decorator.py
+++ decorator.py
@@ -4,24 +4,22 @@ Task 2
 """
 
 
-def make_hashable(*args):
-    return hash(str(args))
+def make_hashable(*args, **kwargs):
+    return hash(str(args) + str(kwargs))
 
 
 def caching(use_cache=True):
     def caching_decorator(f):
         cache = dict()
 
-        def cache_function(*data):
+        def cache_function(*data, **kwdata):
             if use_cache is True:
-                new_data = make_hashable(data)
+                new_data = make_hashable(data, kwdata)
                 if new_data not in cache:
-                    cache[new_data] = f(*data)
-                    return cache[new_data]
-                else:
-                    return cache[new_data]
+                    cache[new_data] = f(*data, **kwdata)
+                return cache[new_data]
             else:
                 print('no cache')
-                return f(*data)
+                return f(*data, **kwdata)
         return cache_function
     return caching_decorator
