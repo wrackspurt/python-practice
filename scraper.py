3471892b71b277f28e6d8f7b7581dcf9c7f79857
Lisa <fanciful.mischief@gmail.com>
2020-03-19 18:50:41 +0300

    fixed scraper.py

--- scraper.py
+++ scraper.py
@@ -10,9 +10,9 @@ import requests
 from lxml import html
 
 
-url = 'http://www.podrastayka.ru/'
-menu = '//div[@id = "bar1"]//a[@class="linkpodmenu"]'
-cosmetics = url + 'index.php?categoryID=291'
+URL = 'http://www.podrastayka.ru/'
+MENU = '//div[@id = "bar1"]//a[@class="linkpodmenu"]'
+COSMETICS = URL + 'index.php?categoryID=291'
 
 
 def get_submenu_urls(menu_path):
@@ -23,7 +23,7 @@ def get_submenu_urls(menu_path):
 
 
 def get_bargains_from_submenu(path):
-    request = requests.get(url + path)
+    request = requests.get(URL + path)
     body = html.fromstring(request.text)
     prices = body.xpath('//div[@style="font-size:11px;"]')
     results = []
@@ -31,7 +31,7 @@ def get_bargains_from_submenu(path):
         if p.xpath('.//div[@class="prod_price"]/strike/text()'):
             price = p.xpath('.//div[@class="prod_price"]/text()')
             elem = p.xpath('.//div[@class="prod_descr"]/text()')
-            results.append(str(elem[0]).strip() + ': ' + str(price[0]).strip())
+            results.append(f'{str(elem[0]).strip()}: {str(price[0]).strip()}')
     return results
 
 
@@ -41,7 +41,7 @@ def get_urls(path):
     urls = []
     links = body.xpath('//a[@class="linkpodmenucategory"]')
     for l in links:
-        urls.append(url + l.attrib['href'])
+        urls.append(URL + l.attrib['href'])
     return urls
 
 
@@ -56,32 +56,32 @@ def get_bargains_from_subcategory(path):
             if p.xpath('.//div[@class="prod_price"]/strike/text()'):
                 price = p.xpath('.//div[@class="prod_price"]/text()')
                 elem = p.xpath('.//div[@class="prod_descr"]/text()')
-                results.append(str(elem[0]).strip() + ': ' + str(price[0]).strip())
+                results.append(f'{str(elem[0]).strip()}: {str(price[0]).strip()}')
     return results
 
 
 if __name__ == '__main__':
-    response = requests.get(url)
+    response = requests.get(URL)
 
     tree = html.fromstring(response.text)
 
-    submenu = tree.xpath(menu + '/text()')
+    submenu = tree.xpath(MENU + '/text()')
 
-    print('привет! вот категории товаров интернет-магазина ' + url[11:25] + ': ')
+    print(f'привет! вот категории товаров интернет-магазина {URL[11:25]}: ')
     for i in range(0, len(submenu)):
-        print(i + + 1, '-', submenu[i])
+        print(i + 1, '-', submenu[i])
 
     try:
         user = int(input('\nпожалуйста, выбери одну из них (просто введи номер категории): '))
 
         choice = {
-            1: get_bargains_from_subcategory(url + get_submenu_urls(menu)[0]),
-            2: get_bargains_from_subcategory(url + get_submenu_urls(menu)[1]),
-            3: get_bargains_from_submenu(get_submenu_urls(menu)[2]),
-            4: get_bargains_from_submenu(get_submenu_urls(menu)[3]),
-            5: get_bargains_from_submenu(get_submenu_urls(menu)[4]),
-            6: get_bargains_from_submenu(get_submenu_urls(menu)[5]),
-            7: get_bargains_from_subcategory(cosmetics)
+            1: get_bargains_from_subcategory(URL + get_submenu_urls(MENU)[0]),
+            2: get_bargains_from_subcategory(URL + get_submenu_urls(MENU)[1]),
+            3: get_bargains_from_submenu(get_submenu_urls(MENU)[2]),
+            4: get_bargains_from_submenu(get_submenu_urls(MENU)[3]),
+            5: get_bargains_from_submenu(get_submenu_urls(MENU)[4]),
+            6: get_bargains_from_submenu(get_submenu_urls(MENU)[5]),
+            7: get_bargains_from_subcategory(COSMETICS)
         }
 
         print('\nв данной категории имеются следующие товары по скидке: ')
@@ -90,7 +90,7 @@ if __name__ == '__main__':
             print('упс! кажется, в этой категории нет товаров по скидке.')
         else:
             for c in range(0, len(choice[user])):
-                print(c + 1, ')', choice[user][c], sep='')
+                print(f'{c + 1}) {choice[user][c]}')
     except KeyError:
         print('такой категории в списке нет.')
     except ValueError:
